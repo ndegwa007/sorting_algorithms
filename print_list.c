7fc648933e0d772e2382974d472055b3d8425366
Mamsana <alicemamsana@gmail.com>
2023-01-23 00:58:26 -0800

    updating betty errors

--- print_list.c
+++ print_list.c
@@ -1,3 +1,6 @@
+#include <stdio.h>
+#include "sort.h"
+
 /**
  * print_list - Prints a list of integers
  *
@@ -5,16 +8,16 @@
  */
 void print_list(const listint_t *list)
 {
-    int i;
+	int i;
 
-    i = 0;
-    while (list)
-    {
-        if (i > 0)
-            printf(", ");
-        printf("%d", list->n);
-        ++i;
-        list = list->next;
-    }
-    printf("\n");
+	i = 0;
+	while (list)
+	{
+		if (i > 0)
+			printf(", ");
+		printf("%d", list->n);
+		++i;
+		list = list->next;
+	}
+	printf("\n");
 }
