1d43c5c41e43fc9f0760618917f055221f14e5b9
Mamsana <alicemamsana@gmail.com>
2023-01-23 02:04:25 -0800

    update header file

--- sort.h
+++ sort.h
@@ -1,10 +1,12 @@
 #ifndef SORT_H
 #define SORT_H
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <stdbool.h>
+
 /**
- * struct listint_s: Doubly linked list node
+ * struct listint_s - Doubly linked list node
  * @n: Integer stored in the node
  * @prev: pointer to the previous node
  * @next: pointer to the next nod
@@ -15,7 +17,7 @@ typedef struct listint_s
 	const int n;
 	struct listint_s *prev;
 	struct listint_s *next;
-}listint_t;
+} listint_t;
 
 void print_array(const int *array, size_t size);
 void print_list(const listint_t *list);
