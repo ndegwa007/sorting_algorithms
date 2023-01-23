6d8d42cfad17936287de122f55b7c7c0f3199263
ndegz <ndegz@example.com>
2023-01-22 20:48:18 -0800

    modified bubble

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -11,24 +11,16 @@ void bubble_sort(int *array, size_t size)
 {
 	size_t i, j, temp;
 
-	if (sizeof(array) / sizeof(array[0]) <= 1)
-		print_array(array, size)
-
 	for (i = 0; i < size; i++)
 	{
 		for (j = 0; j < size - i - 1; j++)
 		{
-			bool swapped = false;
 
 			if (array[j] > array[j + 1])
 			{
 				temp = array[j];
 				array[j] = array[j + 1];
 				array[j + 1] = temp;
-				swapped = true;
-			}
-			if (swapped == true)
-			{
 				print_array(array, size);
 			}
 		}
