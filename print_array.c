090af5b45618b6944f27aa5dd42734dca7fd31ac
ndegz <ndegz@example.com>
2023-01-23 01:21:13 -0800

    changed base files

--- print_array.c
+++ print_array.c
@@ -1,23 +1,24 @@
 #include "sort.h"
 
-/*
- * print_array - print an array of integers
- * @array: the array ro be printed
- * @size: size of the @array
+/**
+ * print_array - Prints an array of integers
+ *
+ * @array: The array to be printed
+ * @size: Number of elements in @array
  */
 
 void print_array(const int *array, size_t size)
 {
-	size_t i = 0;
+	size_t i;
 
-	while(array && i < size)
+	i = 0;
+
+	while (array && i < size)
 	{
-		if(i > 0)
-		{
+		if (i > 0)
 			printf(", ");
-		}
 		printf("%d", array[i]);
 		i++;
 	}
 	printf("\n");
-}			
+}
